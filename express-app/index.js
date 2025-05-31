6d82d803710a9ddcbf8be72c070847e6b323d7ab
xxxxxemirxxxxx <emirhadziomerovicaws@gmail.com>
2025-05-31 14:57:51 +0200

    Update index.js

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Merhaba my friends!</h1>')
+  res.send('<h1>Merhaba my friendo!!!</h1>')
 })
 
 const PORT = 8080
